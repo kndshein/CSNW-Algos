a33150e290f77de303f9b950909e0f1473b85684
Kaung Shein <nicholasling1412@gmail.com>
2021-08-16 12:30:30 -0400

    Init commit with questions

---
+++ euroEuroBillsYall.js
@@ -0,0 +1,1 @@
+//* Euro coins are denominated as follows: 1, 2, 5, 10, 20 and 50 cent, €1 and €2. Given this, how many different ways can change be made for a €5 note? Write a program to answer this question.
---
+++ mindBoggle.js
@@ -0,0 +1,1 @@
+//* Given a 4x4 matrix of letters chosen randomly from A-Z (with replacement), and a list L of english words (you can use any words you’d like). Write a program to output all the words present in the matrix per the rules of Boggle. Your program should be able to accept pre-set 4x4 matrices and word lists in a reasonable (text-based) format of your choosing.
