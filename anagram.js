2ffba598e0c54da8199e33e64567bf77edb0612d
Kaung Shein <nicholasling1412@gmail.com>
2021-08-16 12:54:58 -0400

    added initial thoughts

--- anagram.js
+++ anagram.js
@@ -1,5 +1,7 @@
 // TODO: Euro coins are denominated as follows: 1, 2, 5, 10, 20 and 50 cent, €1 and €2. Given this, how many different ways can change be made for a €5 note? Write a program to answer this question.
 
+// Initial thought process: I have done this algorithm before. I can either sort the strings and check if they're equal or create a hash table for it. Ez pz.
+
 const word1 = "orchestra",
   word2 = "carthorse",
   word3 = "rail safety",
