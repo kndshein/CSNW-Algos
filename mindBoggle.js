f52b0e07314d8beacbafe80bea0980a00978f99a
Kaung Shein <nicholasling1412@gmail.com>
2021-08-16 17:39:30 -0400

    Refactored + coords table set up

--- mindBoggle.js
+++ mindBoggle.js
@@ -4,25 +4,54 @@ const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 const size = 4;
 const wordsList = ["cat", "what", "words", "alarm"];
 
-const generateMatrix = (size) => {
-  let matrix = [];
+const generateMatrix = (matrix, size, coords) => {
   for (let i = 0; i < size; i++) {
     matrix.push([]);
     for (let j = 0; j < size; j++) {
       const randomLetter =
         alphabets[Math.floor(Math.random() * alphabets.length)];
       matrix[i].push(randomLetter);
+      if (coords[randomLetter]) {
+        coords[randomLetter].push([i, j]);
+      } else {
+        coords[randomLetter] = [[i, j]];
+      }
     }
   }
-  return matrix;
+};
+
+const getCoords = (matrix, coords) => {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (coords[matrix[i][j]]) {
+        coords[matrix[i][j]].push([i, j]);
+      } else {
+        coords[matrix[i][j]] = [[i, j]];
+      }
+    }
+  }
 };
 
 const boggle = (size, wordsList, preset) => {
   let matrix = [],
-    results = [];
-  !preset ? (matrix = generateMatrix(size)) : (matrix = preset);
-  let words = wordsList.filter((e) => e.length > 3);
-  console.log(words);
+    results = [],
+    coords = {};
+
+  // Check if there's preset matrix
+  if (preset) {
+    matrix = preset;
+    getCoords(matrix, coords);
+  } else {
+    generateMatrix(matrix, size, coords);
+  }
+
+  wordsList = wordsList.filter((e) => e.length > 3);
+  console.log(matrix, coords);
 };
 
-boggle(4, wordsList);
+boggle(4, wordsList, [
+  ["V", "R", "Y", "B"],
+  ["O", "F", "U", "M"],
+  ["A", "K", "P", "T"],
+  ["D", "M", "J", "U"],
+]);
